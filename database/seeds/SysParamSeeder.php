3c01592c6d6017383b4f839206a21954509cb9ae
Andragon <oscarcamarena31@hotmail.com>
2020-07-26 13:21:47 -0600

    OACP
    Cambios sysparam

--- database/seeds/SysParamSeeder.php
+++ database/seeds/SysParamSeeder.php
@@ -17,6 +17,9 @@ class SysParamSeeder extends Seeder
         $sys_param->created_at = Carbon::now();
         $sys_param->updated_at = Carbon::now();
         $sys_param->session_time = 200;
+        $sys_param->app_name = 'Chol-app';
+        $sys_param->version = '1.0.0';
+        $sys_param->sys_date = Carbon::now();
         $sys_param->save();
     }
 }
